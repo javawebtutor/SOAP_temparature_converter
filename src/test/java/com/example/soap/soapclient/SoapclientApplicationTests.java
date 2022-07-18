c3ba6c3cc8bf7e9cab413cd3ed3b28e64be02f91
mrajnitro <mrajnitro@gmail.com>
2022-07-18 19:54:07 +0530

    first commit

---
+++ src/test/java/com/example/soap/soapclient/SoapclientApplicationTests.java
@@ -0,0 +1,12 @@
+/*
+ * package com.example.soap.soapclient;
+ * 
+ * import org.junit.jupiter.api.Test; import
+ * org.springframework.boot.test.context.SpringBootTest;
+ * 
+ * @SpringBootTest class SoapclientApplicationTests {
+ * 
+ * @Test void contextLoads() { }
+ * 
+ * }
+ */
